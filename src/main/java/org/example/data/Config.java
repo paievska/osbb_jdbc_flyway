85ccd2b5d551fac63453e7fc90de18d697cf247d
Diana Paievska <71642076+paievska@users.noreply.github.com>
2023-09-15 22:19:22 +0300

    add class Config.java

---
+++ src/main/java/org/example/data/Config.java
@@ -0,0 +1,10 @@
+package org.example.data;
+
+public class Config {
+    private Config() {
+    }
+
+    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/osbb";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "dianafunny";
+}
